211f0951815a18e9e9abb9554a528aeadcdd0c76
Baptiste Masson <baptiste.masson@hotmail.fr>
2025-06-25 13:49:57 +0200

    associations completed with dependencies - check comments for explanation

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,12 @@
 class Instrument < ApplicationRecord
   belongs_to :user
+
+  # An instrument can be booked many times and the bookings will persist if the instrument is deleted for historical records.
+  # This is useful for keeping track of past bookings even if the instrument is deleted.
+  has_many :bookings
+
+  # An instrument can receive many reviews, all will be destroyed if the instrument is deleted.
+  has_many :reviews, dependent: :destroy
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts, dependent: :destroy
+  # A user can propose many instruments and make many bookings which will be destroyed if the user is deleted.
+  has_many :instruments, dependent: :destroy
+
+  # A user can book many instruments, and the bookings will persist if the user is deleted (for historical records).
+  # This is useful for keeping track of past bookings even if the user account is deleted.
+  has_many :bookings
+
+  # A user can write many reviews, which will persist if the user is deleted.
+  has_many :reviews
 end
