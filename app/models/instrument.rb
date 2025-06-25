a8cf2170795ecb9af7a27b2c68401d48f35f1f94
Baptiste Masson <baptiste.masson@hotmail.fr>
2025-06-25 14:15:34 +0200

    validations for instruments + change in seed file to allow the seeds to pass validations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -8,5 +8,16 @@ class Instrument < ApplicationRecord
   # An instrument can receive many reviews, all will be destroyed if the instrument is deleted.
   has_many :reviews, dependent: :destroy
 
+  # Validations
+  # An instrument must have a name, description, size, instrument type, status, and price per day.
+
+  # For instrument types, we use a predefined list to avoid input errors. Add more types to make it exhaustive.
+  validates :instrument_type, presence: true, inclusion: { in: %w[String Woodwind Brass Percussion Electronic] }
+  validates :price_per_day, presence: true, numericality: { greater_than: 0 }
+  validates :size, presence: true, inclusion: { in: %w[Standard Large Compact Mini XL] }
+  validates :description, presence: true, length: { minimum: 10, maximum: 400 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :status, presence: true, inclusion: { in: %w[available booked maintenance] }
+
 
 end
