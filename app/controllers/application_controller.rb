4a42c7a875f8e37f6d0ba42d96fddf6187e38512
Baptiste Masson <baptiste.masson@hotmail.fr>
2025-06-25 18:31:37 +0200

    added new fields for the edit user form and strong params to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,7 @@ class ApplicationController < ActionController::Base
 
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :phone_number])
 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :phone_number])
+
   end
 end
