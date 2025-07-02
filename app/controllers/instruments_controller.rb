784c8ce696bf6d8873c25dede1dd823701979403
barthelemy terrier <barthelemy.terrier@yahoo.fr>
2025-07-02 23:12:47 +0200

    owner bookings and listing destroy

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -9,6 +9,9 @@ class InstrumentsController < ApplicationController
 
   def show
     @instrument = Instrument.find(params[:id])
+    @bookings = @instrument.bookings if current_user == @instrument.user
+  rescue ActiveRecord::RecordNotFound
+    redirect_to instruments_path, alert: 'Instrument not found.'
   end
 
   def new
@@ -29,6 +32,14 @@ class InstrumentsController < ApplicationController
       flash.now[:alert] = 'There was an error while creating the instrument.'
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @instrument = current_user.instruments.find(params[:id])
+    @instrument.destroy
+    redirect_to dashboard_path, notice: 'Instrument supprimé avec succès.'
+  rescue ActiveRecord::RecordNotFound
+    redirect_to dashboard_path, alert: 'Instrument not found or you do not have permission to delete it.'
   end
 
   private
