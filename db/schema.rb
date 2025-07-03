f069d779beb43dc4a13c49b4f313cdde6d8d7959
Jonathan Cucculelli <jonathan.cucculelli04@gmail.com>
2025-07-04 01:48:28 +0200

    modifications faites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_02_192606) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_25_181432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,9 +48,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_02_192606) do
     t.bigint "user_id", null: false
     t.bigint "instrument_id", null: false
     t.integer "total_price"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.index ["instrument_id"], name: "index_bookings_on_instrument_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -70,7 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_02_192606) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.text "content"
+    t.text "comment"
     t.bigint "user_id", null: false
     t.bigint "instrument_id", null: false
     t.datetime "created_at", null: false
